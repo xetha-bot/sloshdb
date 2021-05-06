54dcd2cdc29e14fc7ef13e253a68d97245058a13
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2021-05-06 20:27:40 +0800

    feat(update): more logging

--- xandra/src/scripts/update.ts
+++ xandra/src/scripts/update.ts
@@ -6,48 +6,68 @@ import sha256File from 'sha256-file';
 import fetch from 'node-fetch';
 import { version } from '../../package.json';
 import { exec } from 'child_process';
+import Logger from '../utils/Logger';
 
 const execute = promisify(exec);
 
 (async function () {
+
+    Logger.info(`Checking for Updates...`);
+
     const response = await fetch(
         `https://api.github.com/repos/${Config.owner}/${Config.repository}/releases/latest`,
     );
     const json = await response.json();
 
     if (`v${version}` === json.tag_name) {
-        throw new Error(`Everything is up-to-date.`);
+        Logger.error(`Everything is up-to-date.`);
+        return process.exit(1);
     }
 
+    Logger.info(`New Update Available: ${json.tag_name}`);
+
+    Logger.info('Deleting old files...');
     await fs
         .unlink(path.join(path.resolve(process.cwd()), 'dist'))
-        .catch(() => {});
+        .catch(() => { });
 
     const archive = path.join(path.resolve(process.cwd()), Config.filename);
+
+    Logger.info('Downloading Update...');
+    const download = Logger.startTimer();
 
     await execute(
         `curl -Lo ${Config.filename} https://github.com/${Config.owner}/${Config.repository}/releases/latest/download/${Config.filename}`,
     );
+
+    download.done('Download Completed.');
+
+    Logger.info('Fetching Checksums...');
     const res = await fetch(
         `https://github.com/${Config.owner}/${Config.repository}/releases/latest/download/checksum.txt`,
     );
+
     const stdout = await res.text();
 
     const checksum = sha256File(archive);
     const shasum = stdout.split(' ')[0];
 
+    Logger.info('Validating Checksums...');
     if (shasum !== checksum) {
         throw new Error(
             `SHA256 Checksum failed. Expected to be '${shasum}' but gotten '${checksum}'`,
         );
     }
+    Logger.info(`Checksums Matched!`);
+
+    Logger.info('Installing Update...');
+    const install = Logger.startTimer();
 
     await execute(`tar -xzf ${Config.filename}`);
     await fs.unlink(archive);
-
     await execute(`npm install --production`);
 
-    console.log('Updated.');
+    install.done('Installation Completed.');
 
     process.exit();
 })().catch((err) => {
